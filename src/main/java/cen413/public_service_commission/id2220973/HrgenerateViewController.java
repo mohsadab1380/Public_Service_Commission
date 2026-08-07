f3ab4e95876f35aea7851223abf09bc01d1e5b08
mohsadab <mohammadshadab15679@gmail.com>
2026-08-07 21:42:45 +0600

    Implement Controller classes and coding

--- src/main/java/cen413/public_service_commission/id2220973/HrgenerateViewController.java
+++ src/main/java/cen413/public_service_commission/id2220973/HrgenerateViewController.java
@@ -1,15 +1,51 @@
 package cen413.public_service_commission.id2220973;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class HrgenerateViewController
 {
+
     @javafx.fxml.FXML
-    private TextField reportPeriodTF;
+    private ComboBox<String> reportPeriodCB;
+    @javafx.fxml.FXML
+    private Label verifyingRecruitmentRecordsForSelectedPeriodLabel;
+    @javafx.fxml.FXML
+    private Label processingRecruitmentStatisticsLabel;
+    @javafx.fxml.FXML
+    private Label exportConfirmationAlertLabel;
+    @javafx.fxml.FXML
+    private Label checkingReportParametersLabel;
+    @javafx.fxml.FXML
+    private TextArea recruitmentReportDisplayViewTA;
+    @javafx.fxml.FXML
+    private ComboBox<String> departmentNameCB;
 
     @javafx.fxml.FXML
     public void initialize() {
+        reportPeriodCB.getItems().addAll("2025", "2026");
+        departmentNameCB.getItems().addAll("BBA", "BSC");
+
     }
 
     @javafx.fxml.FXML
     public void exportReportButtonOA(ActionEvent actionEvent) {
+        exportConfirmationAlertLabel.setText("Exported");
+    }
+
+    @javafx.fxml.FXML
+    public void generateAndProcessReportButtonOA(ActionEvent actionEvent) {
+        checkingReportParametersLabel.setText("Checked");
+        verifyingRecruitmentRecordsForSelectedPeriodLabel.setText("Verified");
+        processingRecruitmentStatisticsLabel.setText("Processed");
+    }
+
+    @javafx.fxml.FXML
+    public void recruitmentReportsButtonOA(ActionEvent actionEvent) {
+        checkingReportParametersLabel.setText("Waiting");
+        verifyingRecruitmentRecordsForSelectedPeriodLabel.setText("Waiting");
+        processingRecruitmentStatisticsLabel.setText("Waiting");
     }
 }

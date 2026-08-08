d54d59732e65725c06a68ec82a220269d63c60a5
mohsadab <mohammadshadab15679@gmail.com>
2026-08-08 21:05:33 +0600

    Updated

--- src/main/java/cen413/public_service_commission/id2220973/EnsuresecurityViewController.java
+++ src/main/java/cen413/public_service_commission/id2220973/EnsuresecurityViewController.java
@@ -1,15 +1,48 @@
 package cen413.public_service_commission.id2220973;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class EnsuresecurityViewController
 {
+
     @javafx.fxml.FXML
-    private TextField examInformationsTF;
+    private Label verifyingSecurityStaffAssignmentsLabel;
+    @javafx.fxml.FXML
+    private Label processingSecurityMonitoringRecordsLabel;
+    @javafx.fxml.FXML
+    private Label confirmationMessageAlertLabel;
+    @javafx.fxml.FXML
+    private ComboBox<String> examInfoCB;
+    @javafx.fxml.FXML
+    private Label checkingMonitoringRequirementsLabel;
+    @javafx.fxml.FXML
+    private TextArea displaysSecurityStatusDashboardTA;
 
     @javafx.fxml.FXML
     public void initialize() {
+        examInfoCB.getItems().addAll("Written", "Viva");
+
     }
 
     @javafx.fxml.FXML
     public void acknowledgeSecurityReportButtonOA(ActionEvent actionEvent) {
+        confirmationMessageAlertLabel.setText("Confirmed");
+    }
+
+    @javafx.fxml.FXML
+    public void securityMonitoringButtonOA(ActionEvent actionEvent) {
+        checkingMonitoringRequirementsLabel.setText("Waiting");
+        verifyingSecurityStaffAssignmentsLabel.setText("Waiting");
+        processingSecurityMonitoringRecordsLabel.setText("Waiting");
+    }
+
+    @javafx.fxml.FXML
+    public void processSecurityMonitoringButtonOA(ActionEvent actionEvent) {
+        checkingMonitoringRequirementsLabel.setText("Checked");
+        verifyingSecurityStaffAssignmentsLabel.setText("Verified");
+        processingSecurityMonitoringRecordsLabel.setText("Processed");
     }
 }
--- src/main/java/cen413/public_service_commission/id2220973/GenerateperformanceViewController.java
+++ src/main/java/cen413/public_service_commission/id2220973/GenerateperformanceViewController.java
@@ -1,15 +1,47 @@
 package cen413.public_service_commission.id2220973;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class GenerateperformanceViewController
 {
+
     @javafx.fxml.FXML
-    private TextField reportingPeriodTF;
+    private ComboBox<String> reportingPeriodCB;
+    @javafx.fxml.FXML
+    private Label verifyingExaminationRecordsAvailabilityLabel;
+    @javafx.fxml.FXML
+    private Label checkingReportingPeriodValidityLabel;
+    @javafx.fxml.FXML
+    private Label downloadableReportFileAlertLabel;
+    @javafx.fxml.FXML
+    private Label processingExaminationAndAttendanceDataLabel;
+    @javafx.fxml.FXML
+    private TextArea displaysCentrePerformanceReportTA;
 
     @javafx.fxml.FXML
     public void initialize() {
+        reportingPeriodCB.getItems().addAll("Morning", "Evening");
     }
 
     @javafx.fxml.FXML
     public void exportReportButtonOA(ActionEvent actionEvent) {
+        downloadableReportFileAlertLabel.setText("Exported");
+    }
+
+    @javafx.fxml.FXML
+    public void processPerformanceReportsButtonOA(ActionEvent actionEvent) {
+        checkingReportingPeriodValidityLabel.setText("Checked");
+        verifyingExaminationRecordsAvailabilityLabel.setText("Verified");
+        processingExaminationAndAttendanceDataLabel.setText("Processed");
+    }
+
+    @javafx.fxml.FXML
+    public void performanceReportsButtonOA(ActionEvent actionEvent) {
+        checkingReportingPeriodValidityLabel.setText("Waiting");
+        verifyingExaminationRecordsAvailabilityLabel.setText("Waiting");
+        processingExaminationAndAttendanceDataLabel.setText("Waiting");
     }
 }

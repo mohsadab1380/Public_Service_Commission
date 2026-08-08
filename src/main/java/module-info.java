6799c1b699b762a0ab76da0e9eb8792adaaffd27
Tahmid Alam <tahmidalam128@gmail.com>
2026-08-08 12:34:55 +0600

    Updated

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,6 +12,4 @@ module cen413.public_service_commission {
 
     opens cen413.public_service_commission to javafx.fxml;
     exports cen413.public_service_commission;
-    exports cen413.public_service_commission.Nahian_Anjum_Rahy;
-    opens cen413.public_service_commission.Nahian_Anjum_Rahy to javafx.fxml;
 }
